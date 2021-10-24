69116b5f0125fc5d5453d6c778bed938b3989bc1
jimmy <jimmy.galland@celjim.fr>
2021-10-24 21:49:56 +0200

    update install-acpi.sh / and Test V7 bromolow OK

--- acpid/install-acpi.sh
+++ acpid/install-acpi.sh
@@ -4,8 +4,9 @@ tar -zxvf pwrbtn_pkg.tgz -C /tmpRoot/
 
 chmod +x /tmpRoot/usr/local/etc/rc.d/S30acpid.sh
 chmod +x /tmpRoot/bin/acpid
+chmod +x /tmpRoot/etc/acpi/powerbtn.sh
 
 /tmpRoot/usr/local/etc/rc.d/S30acpid.sh start
-
+echo "Started acpid"
 exit 0
 
